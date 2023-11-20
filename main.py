972ca72ebd901dcb91e6931c7e2492d954279925
Thais <rotinasoftware@gmail.com>
2023-11-20 20:17:00 -0300

    Mostrando resultado

--- main.py
+++ main.py
@@ -1,4 +1,18 @@
 import json
+from invertion import MergeAndInverte
+
+minhas_preferencia = {
+    "respostas": {
+        "Limpeza di\u00e1ria": 2,
+        "Dividir gastos com mercado": 1,
+        "Pet": 5,
+        "Dormir antes das 23h": 5,
+        "Receber visitas": 5,
+        "Ouvir m\u00fasica alta": 1,
+        "Fazer barulho": 2
+        }
+    }
+
 
 def nova_entrevista():
     nome = input("Digite o nome do candidato: ")
@@ -24,8 +38,34 @@ def nova_entrevista():
         json.dump({ 'nome': nome, 'telefone': telefone, 'respostas': respostas }, arquivo)
         arquivo.write('\n')
 
+def calcula_resultado():
+    with open('entrevistas.json', 'r') as arquivo:
+        entrevistas = [json.loads(line) for line in arquivo]
+
+    min_inversao = 7
+    pessoa_com_maior_pontuacao = None
+    
+    meus_pontos = [ x for x in minhas_preferencia['respostas'].values()]
+    inversion = MergeAndInverte(meus_pontos)
+    my_inversion = inversion.inversion
+
+    for entrevista in entrevistas:
+        lista_pontos = [ x for x in entrevista['respostas'].values()]
+        inversion = MergeAndInverte(lista_pontos)
+        candidate_inversion = inversion.inversion
+        
+        pontuacao = abs(my_inversion - candidate_inversion)
+
+        if pontuacao <= min_inversao:
+            min_inversao = pontuacao
+            pessoa_com_maior_pontuacao = entrevista
+
+    return pessoa_com_maior_pontuacao
+
 def ver_resultados():
-    pass
+    melhor_match = calcula_resultado()
+    print("A melhor opção é: ")
+    print(f"Nome: {melhor_match['nome']} - telefone: {melhor_match['telefone']}")
 
 while True:
     print("\nMenu:")
@@ -36,11 +76,15 @@ while True:
     escolha = input("Escolha a opção (1/2/3): ")
 
     if escolha == '1':
+        print("")
         nova_entrevista()
     elif escolha == '2':
+        print("")
         ver_resultados()
+        break
     elif escolha == '3':
         print("Saindo do programa. Até mais!")
+        print("")
         break
     else:
         print("Opção inválida. Tente novamente.")
